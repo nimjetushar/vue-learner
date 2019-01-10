db544d190920d9c2f7ed826c2c684d8043a69c27
tusharnimje21@gmail.com <tusharnimje21@gmail.com>
2019-01-10 20:36:32 +0530

    initial commit

---
+++ src/components/table/Table.vue
@@ -0,0 +1,37 @@
+<template>
+  <table id='table-component'>
+    <thead>
+      <tr>
+        <th v-for='(col, index) in columns' :key="index">
+          {{ col.name }}
+        </th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for='(item, index) in data' :key="index">
+        <td v-for='(col, idx) in columns' :key="idx">{{item[col.field]}}</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+export default {
+  template: '#table-component',
+  name: 'Table',
+  props: {
+    data: {
+      type: Array,
+      required: true
+    },
+    columns: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+  @import './table.style.scss'
+</style>
---
+++ src/views/Table.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <h3>{{ careCategoryName }} Json Config</h3>
+    <Table
+      :data="gridData"
+      :columns="gridColumns"
+    />
+  </div>
+</template>
+
+<script>
+
+import Table from '../components/table/Table.vue'
+import tableMockData from '../mock/table.json'
+
+export default {
+  components: {
+    Table
+  },
+  props: {
+    careCategoryName: {
+      type: String,
+      default: 'Table Component'
+    }
+  },
+  data: function () {
+    let tableData = []
+
+    for (const key in tableMockData) {
+      tableData.push(tableMockData[key])
+    }
+    tableData = tableData.sort((a, b) => {
+      return (a > b) ? 1 : -1
+    })
+
+    return {
+      gridColumns: [
+        {
+          name: 'Name',
+          field: 'displayableName'
+        },
+        {
+          name: 'Order',
+          field: 'order'
+        },
+        {
+          name: 'Primary',
+          field: 'isPrimary'
+        },
+        {
+          name: 'Prescription Data',
+          field: 'isPrescriptionData'
+        }
+      ],
+      gridData: tableData
+    }
+  }
+}
+</script>
